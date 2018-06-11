06b419e4d4aa3c383348a8d32929e81675d16c0f
Caijialinxx <caijialinxx@foxmail.com>
2018-06-11 17:21:08 +0800

    引入jQuery

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,12 +1,13 @@
 import _ from 'lodash';
+import $ from 'jquery';
 
 function component() {
-  var element = document.createElement('div');
+  var element = $('<div></div>');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.html(_.join(['Hello', 'webpack'], ' '));
 
-  return element;
+  return element[0];
 }
 
-document.body.appendChild(component());
+$('body').append(component());
