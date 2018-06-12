e5a8937b17f2c22b393fe952e05f092d184da69b
Caijialinxx <caijialinxx@foxmail.com>
2018-06-12 19:51:31 +0800

    source map、watch、加载css/pictures/fonts

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,15 +1,22 @@
 import _ from 'lodash';
 import $ from 'jquery';
-import x from './myFile';
+import x from './myFile';     // 引入自己的文件
+import './style.css';         // 引入CSS
+import svg from './logo.svg'; // 引入图片
 
 function component() {
   var element = $('<div></div>');
 
   // Lodash, now imported by this script
   element.html(_.join(['Hello', 'webpack'], ' '));
+  element.addClass('hello');
+  // Add the image to div
+  var logo = new Image();
+  logo.src = svg;
+  element.append(logo);
 
   return element[0];
 }
 
 $('body').append(component());
-alert(x());
+console.log(x());
