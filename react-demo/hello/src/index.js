638bfbc4fd47a91d0c486d5e75fc997c907bd74c
Caijialinxx <caijialinxx@foxmail.com>
2018-06-12 20:38:45 +0800

    react小试

---
+++ react-demo/hello/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <h1>Hello, React!</h1>,
+  document.getElementById('root')
+);
+registerServiceWorker();
