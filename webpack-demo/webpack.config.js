dc89b9b991bf4327c8c69e99ddc8b6a1f53f140a
Caijialinxx <caijialinxx@foxmail.com>
2018-06-13 20:11:47 +0800

    loading font

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
       {
         test: /\.(jpg|png|svg|gif)$/,
         use: ['file-loader']
+      },
+      {
+        test: /\.(ttf|woff|woff2|otf|eot)$/,
+        use: ['file-loader']
       }
     ]
   }
