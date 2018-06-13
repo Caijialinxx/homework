2f70548f3e2731d8eab2facec7b65bf964c2f4b2
Caijialinxx <caijialinxx@foxmail.com>
2018-06-13 13:55:35 +0800

    webpack-dev-server

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  devServer: {
+    contentBase: './dist'
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
