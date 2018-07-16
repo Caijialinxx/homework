a506b1ccf31bd4bd34045d4c7c35ce3edf7b00db
Caijialinxx <caijialinxx@foxmail.com>
2018-07-16 14:10:36 +0800

    loading css

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,16 @@
 import _ from "lodash"
+import './style.css'
 
 function component() {
   let span = document.createElement('span')
   span.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  span.className = 'hello'
 
-  return span
+  let wrapper = document.createElement('div')
+  wrapper.appendChild(span)
+  wrapper.className = 'wrapper'
+
+  return wrapper
 }
 
 document.body.appendChild(component())
